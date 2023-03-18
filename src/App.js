36a783ab5c693dbb35be523c0e16b8cb289a7359
kkkhushii <khushijoshi512@gmail.com>
2023-03-18 10:53:40 +0530

    Netflix_Clone

--- src/App.js
+++ src/App.js
@@ -1,57 +1,25 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
-
-import { Layout, Typography, Space } from 'antd';
-import Navbar from './components/Navbar';
-import Homepage from './components/Homepage'
-import Exchanges from './components/Exchanges'
-import Cryptocurrencies from './components/Cryptocurrencies'
-import CryptoDetails from './components/CryptoDetails'
-import News from './components/News';
-import './Home.css';
+import React from 'react';
+import './App.css';
+import Row from './Row';
+import requests from './requests';
+import Banner from './Banner';
+import Nav from './Nav';
 
 function App() {
-  return (
-    <div  className="app">
-      <div className="navbar">
-        <Navbar/>
-
-      </div>
-      <div className="main">
-      <Layout>
-        <div className="routes">
-       
-       
-           <Routes>
-            
-           <Route path="/" element={<Homepage/>}/>
-           <Route path="/exchanges" element={<Exchanges/>}/>
-           <Route path="/cryptocurrencies" element={<Cryptocurrencies/>}/>
-           <Route path="/crypto/:coinId" element={<CryptoDetails/>}/>
-           <Route path="/news" element={<News/>}/>
-
-           </Routes>
-        </div>
-        </Layout>
-        <div className="footer">
-        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2023
-          <NavLink to="/">
-            Cryptoverse Inc.
-          </NavLink> <br />
-          All Rights Reserved.
-        </Typography.Title>
-        <Space>
-          <NavLink className="footer_link" to="/">Home</NavLink>
-          {/* <NavLink to="/exchanges">Exchanges</NavLink> */}
-          <NavLink className="footer_link"  to="/news">News</NavLink>
-        </Space>
-      </div>
-      </div>     
-  </div>
-  );
-
+	return (
+		<div className="app">
+			<Nav />
+			<Banner />
+			<Row title="NETFLIX ORIGINALS" isLargeRow fetchUrl={requests.fetchNetflixOriginals} />
+			<Row title="Trending" fetchUrl={requests.fetchTrending} />
+			<Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+			<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+			<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+		</div>
+	);
 }
 
 export default App;
-// https://github.com/adrianhajdin/project_cryptoverse
-// https://blog.openreplay.com/leverage-data-from-rapidapi-using-redux-toolkit
